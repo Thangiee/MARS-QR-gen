9aeedfcf9560abaf1a102a1bd2c58965cab59e8b
thangiee <thang.le@mavs.uta.edu>
2015-12-01 10:57:45 -0600

    Add Bernae's code

--- build.sbt
+++ build.sbt
@@ -5,7 +5,8 @@ mainClass in assembly := Some("Main")
 assemblyOutputPath in assembly := new File("./QR-gen-gui.jar")
 
 libraryDependencies ++= Seq(
-  "net.glxn.qrgen" % "javase" % "2.0"
+  "net.glxn.qrgen" % "javase" % "2.0",
+  "com.mashape.unirest" % "unirest-java" % "1.4.7"
 )
 
 assemblyOption in assembly := (assemblyOption in assembly).value.copy(includeScala = false)
--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,55 @@
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
 import net.glxn.qrgen.core.image.ImageType;
 import net.glxn.qrgen.javase.QRCode;
 
 import javax.swing.*;
 import java.awt.*;
 import java.io.ByteArrayOutputStream;
+import java.io.*;
+import java.util.Date;
+import java.util.UUID;
 
-public class Main {
+public class Main{
 
     public static void main(String[] args) {
+        String computerName = "";
+        String fileName = "C:\\ProgramData\\mars\\.computerID.txt";
+        String line = null;
+
         JFrame frame = new JFrame("QR code example");
         frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
         frame.setSize(800, 800);
 
+        try{
+            FileReader filereader = new FileReader(fileName);
+            BufferedReader bufferedReader = new BufferedReader(filereader);
+            while((line = bufferedReader.readLine()) != null){
+                computerName = line;
+            }
+            genQR(frame, computerName);
+        }
+        catch(FileNotFoundException ex){
+            JOptionPane.showMessageDialog(frame, "File of computer ID not found. Re-install program.");
+                System.out.println(fileName+" not found");
+        }
+        catch(IOException ex)
+        {
+            ex.printStackTrace();
+        }
+    }
+    public static void genQR(JFrame frame, String output)
+    {
         System.out.println("Generating QR code");
-        ByteArrayOutputStream stream = QRCode.from("Hello World").withSize(256, 256).to(ImageType.JPG).stream();
+        UUID uuid= UUID.randomUUID();
+        try {
+            HttpResponse<JsonNode> jsonResponse = Unirest.post("http://httpbin.org/post").field("uuid", uuid.toString()).asJson();
+        } catch (UnirestException e) {
+            e.printStackTrace();
+        }
+        ByteArrayOutputStream stream = QRCode.from(output).withSize(256, 256).to(ImageType.JPG).stream();
         ImageIcon QRCodeImage = new ImageIcon(stream.toByteArray());
 
         JPanel panel = new JPanel(new BorderLayout());
