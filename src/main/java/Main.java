a6091e11e8f66f807b2c0e9606a129af97c6baca
Thangiee <Thangiee0@gmail.com>
2015-12-04 06:26:33 -0600

    Switch GUI to using javaFX

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,80 +1,24 @@
-import com.mashape.unirest.http.HttpResponse;
-import com.mashape.unirest.http.JsonNode;
-import com.mashape.unirest.http.Unirest;
-import com.mashape.unirest.http.exceptions.UnirestException;
-import net.glxn.qrgen.core.image.ImageType;
-import net.glxn.qrgen.javase.QRCode;
-import javax.swing.JProgressBar;
-import java.awt.BorderLayout;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Stage;
 
-import javax.swing.*;
-import org.json.*;
-import java.io.ByteArrayOutputStream;
-import java.io.*;
-import java.util.UUID;
-
-public class Main{
-
-    public static void main(String[] args) {
-        String computerName = "";
-        String fileName = "C:\\ProgramData\\mars\\.computerID.txt";
-        String line = null;
-
-        JFrame frame = new JFrame("QR code example");
-        frame.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
-        frame.setSize(800, 800);
-
-        try{
-            FileReader filereader = new FileReader(fileName);
-            BufferedReader bufferedReader = new BufferedReader(filereader);
-            while((line = bufferedReader.readLine()) != null){
-                computerName = line;
-            }
-            genQR(frame, computerName);
-        }
-        catch(FileNotFoundException ex){
-            JOptionPane.showMessageDialog(frame, "File of computer ID not found. Re-install program.");
-                System.out.println(fileName+" not found");
-        }
-        catch(IOException ex)
-        {
-            ex.printStackTrace();
-        }
+public class Main extends Application {
+    @Override
+    public void start(Stage primaryStage) throws Exception {
+        Parent root = FXMLLoader.load(getClass().getResource("/main.fxml"));
+        root.getStylesheets().add(Main.class.getResource("/css/main.css").toExternalForm());
+        primaryStage.setTitle("M.A.R.S");
+        primaryStage.setScene(new Scene(root, 400, 482));
+        primaryStage.getIcons().addAll(new Image("image/logo.png"));
+        primaryStage.setResizable(false);
+        primaryStage.show();
     }
-    public static void genQR(JFrame frame, String output)
-    {
-        JButton refresh = new JButton("Refresh");
-        String ttl;
 
-        System.out.println("Generating QR code");
-        UUID uuid= UUID.randomUUID();
-        try {
-            HttpResponse<JsonNode> jsonResponse = Unirest.post("http://52.33.35.165:8080/api/register-uuid").field("uuid", uuid.toString()).asJson();
-            int statusCode = jsonResponse.getStatus();
-            if(statusCode == 200)
-            {
-                JSONObject obj= new JSONObject(jsonResponse.getBody());
-                ttl= obj.getJSONObject("object").get("ttl").toString();
-                String code = uuid + "\n" + output;
-                output = code;
-                System.out.println(output);
-            }
-            else
-            {
-                JOptionPane.showMessageDialog(frame, "Server Error");
-
-            }
-        } catch (UnirestException e) {
-            JOptionPane.showMessageDialog(frame, "Unexpected Error");
-            e.printStackTrace();
-        }
-        ByteArrayOutputStream stream = QRCode.from(output).withSize(256, 256).to(ImageType.JPG).stream();
-        ImageIcon QRCodeImage = new ImageIcon(stream.toByteArray());
-
-        JPanel panel = new JPanel(new BorderLayout());
-        panel.add(new JLabel(QRCodeImage), BorderLayout.CENTER);
-       
-        frame.getContentPane().add(panel);
-        frame.setVisible(true);
+    @Override
+    public void stop() throws Exception {
+        System.exit(0);
     }
 }
---
+++ src/main/java/MainController.java
@@ -0,0 +1,115 @@
+import com.jfoenix.controls.JFXButton;
+import com.jfoenix.controls.JFXDialog;
+import com.jfoenix.controls.JFXDialogLayout;
+import com.jfoenix.controls.JFXProgressBar;
+import com.mashape.unirest.http.HttpResponse;
+import com.mashape.unirest.http.JsonNode;
+import com.mashape.unirest.http.Unirest;
+import com.mashape.unirest.http.exceptions.UnirestException;
+import javafx.application.Platform;
+import javafx.fxml.FXML;
+import javafx.geometry.Insets;
+import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.layout.*;
+import net.glxn.qrgen.core.image.ImageType;
+import net.glxn.qrgen.javase.QRCode;
+import org.json.JSONObject;
+
+import java.io.BufferedReader;
+import java.io.ByteArrayInputStream;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.UUID;
+
+public class MainController {
+
+    @FXML public BorderPane borderPane;
+    @FXML public ImageView QRImage;
+    @FXML public JFXProgressBar progressBar;
+    @FXML public StackPane stackPane;
+    @FXML private JFXButton genBtn;
+    @FXML public Label expiredLabel;
+
+    @FXML private void initialize() {
+        expiredLabel.setVisible(false);
+        genQRCode(); // gen a QR code at startup
+        genBtn.setOnAction(event -> genQRCode());
+    }
+
+    private void showMessage(String title, String msg) {
+        JFXButton okBtn = new JFXButton("Ok");
+        okBtn.getStyleClass().add("dialog-accept");
+
+        JFXDialogLayout layout = new JFXDialogLayout();
+        layout.setHeading(new Label(title));
+        layout.setBody(new Label(msg));
+        layout.setActions(okBtn);
+
+        JFXDialog dialog = new JFXDialog(borderPane, layout, JFXDialog.DialogTransition.CENTER);
+        okBtn.setOnAction(event -> dialog.close());
+        dialog.show(stackPane);
+    }
+
+    private void genQRCode() {
+        genBtn.setDisable(true); // disable btn to avoid spamming requests to the server
+
+        // Run slow operations in background thread to avoid blocking the UI thread (causes UI lag).
+        // Any update to UI elements must uses Platform.runLater(..) to do so or nothing will happen.
+        new Thread(() -> {
+            try {
+                Thread.sleep(250); // let ripple animation finish
+
+                String fileName = "C:\\ProgramData\\mars\\.computerID.txt";
+                FileReader filereader = new FileReader(fileName);
+                BufferedReader bufferedReader = new BufferedReader(filereader);
+                String computerName = bufferedReader.readLine();
+
+                final String uuid = UUID.randomUUID().toString();
+                HttpResponse<JsonNode> response = Unirest.post("http://52.33.35.165:8080/api/register-uuid").field("uuid", uuid).asJson();
+                switch (response.getStatus()) {
+                    case 200:
+                        // animate the progress bar to show how much time until the QR code expire
+                        new Thread(() -> {
+                            JSONObject obj = new JSONObject(response.getBody());
+                            final int ttl = obj.getJSONObject("object").getInt("ttl");
+                            final int waitTime = 17; // millis; ~60fps
+                            int timeLeft = ttl;
+
+                            while (timeLeft >= 0) {
+                                try {
+                                    Thread.sleep(waitTime);
+                                } catch (InterruptedException e) {
+                                    e.printStackTrace();
+                                }
+                                final int finalTimeLeft = timeLeft;
+                                Platform.runLater(() -> progressBar.setProgress((double) (finalTimeLeft - waitTime) / ttl));
+                                timeLeft = timeLeft - waitTime;
+                            }
+
+                            Platform.runLater(() -> {
+                                expiredLabel.setVisible(true);
+                                progressBar.setProgress(0);
+                                genBtn.setDisable(false);
+                            });
+                        }).start();
+
+                        // display the QR code with the uuid and computer name embedded
+                        byte[] imageBytes = QRCode.from(uuid + "\n" + computerName).withSize(368, 368).to(ImageType.JPG).stream().toByteArray();
+                        ByteArrayInputStream imageStream = new ByteArrayInputStream(imageBytes);
+                        Platform.runLater(() -> QRImage.setImage(new Image(imageStream)));
+                        break;
+                    // todo: handle other status codes?
+                    default:
+                        Platform.runLater(() -> showMessage(String.valueOf(response.getStatus()), response.getStatusText()));
+                }
+            } catch (UnirestException | InterruptedException e) {
+                Platform.runLater(() -> showMessage("Error!", "System has encountered an unexpected error."));
+                e.printStackTrace();
+            } catch (IOException e) {
+                Platform.runLater(() -> showMessage("Error!", "File of computer ID not found. Try re-install the program."));
+            }
+        }).start();
+    }
+}
